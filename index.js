9422d5cab600a7a88964a9235a05e07e8c04e9cc
Rodney Hinson <rodneyhinson22@gmail.com>
2024-10-15 09:10:13 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+
+
+// Create an express app
+const app = express();
+
+// Use middlewares
+app.use(express.json())
+app.use(cors())
+
+
+
+
+
+
+
+
+// Listen for incoming requests
+app.listen(8080, () => {
+    console.log("App is listening on port 3000");
+});
+
+
