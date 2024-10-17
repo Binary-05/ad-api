1a3d3efb745d9ba45615ecc7730625b035ffc3ee
Rodney Hinson <rodneyhinson22@gmail.com>
2024-10-17 09:24:27 +0000

    modified js files

---
+++ models/adverts.js
@@ -0,0 +1,16 @@
+import { Schema, model } from "mongoose";
+import { toJSON } from "@reis/mongoose-to-json";
+
+const advertSchema = new Schema({
+    title: { type: String, required: true },
+    media: { type: String, required: true },
+    description: { type: String, required: true },
+    price: { type: String, required: true },
+    category: { type: String, required: true }
+}, {
+    timestamps: true
+});
+
+advertSchema.plugin(toJSON);
+
+export const AdvertModel = model("User", advertSchema);
