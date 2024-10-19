e30ccb96f1a53f0e69215d007ed391b712879d2c
Rodney Hinson <rodneyhinson22@gmail.com>
2024-10-19 20:43:28 +0000

    js files modified

---
+++ validators/adverts.js
@@ -0,0 +1,18 @@
+import Joi from "joi";
+
+export const addAdvertValidator = Joi.object({
+    title: Joi.string().required(),
+    media: Joi.string().required(),
+    description: Joi.string().required(),
+    price: Joi.string().required(),
+    category: Joi.string().required()
+});
+
+export const updateAdvertValidator = Joi.object({
+    title: Joi.string(),
+    media: Joi.string(),
+    description: Joi.string(),
+    price: Joi.string(),
+    category: Joi.string()
+});
+
